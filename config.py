71f34978125ad3c09d760b580c62b154f7781962
hermesow70-web <hermesow70@gmail.com>
2026-03-09 23:05:47 +0400

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+# ========== ТВОИ ДАННЫЕ ==========
+BOT_TOKEN = "8678152372:AAHEqZ5Lxe6CsSZpX0loPyOioejOFYCTtoI"
+OWNER_ID = 8402407852
+OWNER_TAG = "#крип"
+CHANNEL_LINK = "https://t.me/+arKuZnc9R9hhNDIx"
+# =================================
